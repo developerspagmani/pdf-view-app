ebf700258102751deaac7c4f5997d09da107a7f2
developerspagmani <manikandan@spagylo.com>
2023-07-25 15:31:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
+var updatedCourse = {};
 //define the mock data with keys
-const courses = [
 
-  {
-    id: 0,
-    name: 'Full Stack Developement Program',
-    price: '89,999'
-  },
-
-  {
-
-    id: 1,
-    name: 'Python Automation Testing Program',
-    price: '64,999'
-  },
-
-  {
-    id: 2,
-    name: 'UI/UX Program',
-    price: '89,999'
-  }
-
-]
-
-window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-  console.log('added'); // never printed
-  window.flutter_inappwebview.callHandler('eventHandler').then((result) => {
-      courses.push(result)
-      alert(JSON.stringify(result))
+window.addEventListener("flutterInAppWebViewPlatformReady", function (event) {
+  console.log("added"); // never printed
+  window.flutter_inappwebview.callHandler("eventHandler").then((result) => {
+    updatedCourse = result
+    alert(updatedCourse)
+    alert(JSON.stringify(result));
   });
 });
 
 var App = () => {
+  const data = [
+    {
+      id: 0,
+      name: "Full Stack Developement Program",
+      price: "89,999",
+    },
 
-  /* Mapping the courses into a new array of JSX nodes as arrayDataItems */
+    {
+      id: 1,
+      name: "Python Automation Testing Program",
+      price: "64,999",
+    },
 
-  const arrayDataItems = courses.map((course,index) => 
+    {
+      id: 2,
+      name: "UI/UX Program",
+      price: "89,999",
+    },
+  ];
+
+  const [courses, setCourses] = useState(data);
+
+  setCourses(...courses,updatedCourse)
+
+  const arrayDataItems = courses.map((course, index) => (
     <li key={index}>
       <p>{course.name}</p>
       <span>{course.price}</span>
     </li>
-  )
+  ));
 
   return (
     <div className="container">
@@ -54,6 +55,6 @@ var App = () => {
       <ul>{arrayDataItems}</ul>
     </div>
   );
-}
+};
 
 export default App;
